3768a15f75fb77d06a4913faee8997d6c4dd5064
Alexander Seleznev <SeleznevRu@gmail.com>
2013-07-20 12:27:27 +0700

    Add relief for buttons

--- extension/defaults/preferences/defaults.js
+++ extension/defaults/preferences/defaults.js
@@ -1,4 +1,5 @@
 pref("extensions.gnome-theme-tweak.newtab-page", false);
 pref("extensions.gnome-theme-tweak.restore-button", false);
+pref("extensions.gnome-theme-tweak.relief-buttons", false);
 pref("extensions.gnome-theme-tweak.tabs-border", false);
 pref("extensions.gnome-theme-tweak.urlbar-history-dropmarker", false);
