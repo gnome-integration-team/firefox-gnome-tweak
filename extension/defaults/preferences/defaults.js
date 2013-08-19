0a593819d75ac7720f6d28e8476a322d68d447f1
Alexander Seleznev <SeleznevRU@gmail.com>
2013-08-20 00:29:39 +0700

    Add "Enable different style for unfocused windows" preference

--- extension/defaults/preferences/defaults.js
+++ extension/defaults/preferences/defaults.js
@@ -1,5 +1,6 @@
 pref("extensions.gnome-theme-tweak.newtab-page", false);
 pref("extensions.gnome-theme-tweak.restore-button", false);
 pref("extensions.gnome-theme-tweak.relief-buttons", false);
+pref("extensions.gnome-theme-tweak.inactive-state", false);
 pref("extensions.gnome-theme-tweak.tabs-border", false);
 pref("extensions.gnome-theme-tweak.urlbar-history-dropmarker", false);
