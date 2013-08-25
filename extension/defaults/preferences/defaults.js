20c6e553ff19126a9a3c27c74eada264c12bdd10
Alexander Seleznev <SeleznevRU@gmail.com>
2013-08-25 21:23:10 +0700

    Add checkbox for "Always show Forward button"

--- extension/defaults/preferences/defaults.js
+++ extension/defaults/preferences/defaults.js
@@ -3,3 +3,4 @@ pref("extensions.gnome-theme-tweak.restore-button", false);
 pref("extensions.gnome-theme-tweak.relief-buttons", false);
 pref("extensions.gnome-theme-tweak.tabs-border", false);
 pref("extensions.gnome-theme-tweak.urlbar-history-dropmarker", false);
+pref("extensions.gnome-theme-tweak.forward-button", false);
